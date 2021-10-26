2c74a02a90b2221f759395337397b3f1b024862a
Virginia-Ordoño-Bernier <viorbe20@gmail.com>
2021-10-26 23:40:45 +0200

    ex. verbs: html form creation and data validation

--- ud3/ud3_act_form/ej5/index.php
+++ ud3/ud3_act_form/ej5/index.php
@@ -8,82 +8,20 @@
  * http://192.168.10.10/ud3/ud3_act_form/ej5/
  */
 
- //Estilo
- echo <<<EOD
- <style type='text/css'>
- </style>
- EOD;
-
- //Enunciado
- echo <<<EOD
- <h1>Test Verbos Irregulares en Inglés</h1>
- EOD;
-
 // //Archivo que contiene array con los verbos irregulares
 require __DIR__ . "/php/getIrregularVerbs.php";
 
-$difficultyLevel = 2;
-$verbsAmountInput = 6;
 $a_indexes = array();
 
-
- /**
-  * Función que genera y muestra 20 números para que el usuario elija cantidad de verbos
-  */
-function randomNumbers() {
-   echo ("<label for='verbsSelection'>Elige la cantidad de verbos </label>");
-   echo ("<select name='verbsSelection' id='verbsSelection'>");
-   
-   //Generamos el número máximo de verbos que se pueden mostrar
-   for ($i = 1; $i <= 20; $i++) {
-      echo ("<option value='numberSelected'" . $i . "'>" . $i . "</option>");
-   }
-   echo ("</select><br>");
-};
-
- /**
-  * Función que muestra el nivel de dificulta del test
-  */
-  function difficultyLevel() {
-   echo('<br>');
-   echo ("<label for='level'>Elige el nivel según dificultad </label>");
-   echo ("<select name='level' id='level'>");
-   
-   //Mostramos niveles de dificultad
-      echo ("<option value='level1'>1. Completar un hueco</option>");
-      echo ("<option value='level2'>2. Completar dos huecos</option>");
-      echo ("<option value='level3'>3. Completar tres huecos</option>");
-      echo ("<option value='level4'>4. Completar todos los huecos</option>");
-      echo ("</select><br>");
-};
-
-// /**
-//  * Función que envía el formulario inicial
-//  */
-// function submitSelection(){
-//    echo("<br><input type='submit' value='Enviar' id='submit'>");
-// };
-
-// /**
-//  * Función que invoca las funciones necesarias para mostrar el primer formulario por pantalla
-//  */
-// function showFirstForm(){
-//    randomNumbers();
-//    difficultyLevel();
-//    submitSelection();
-// };
-
-// //Salida por pantalla
-// showFirstForm();
-
-
+$a_selectedGaspsAmount;
+$a_selectedVerbsAmount;
 
 /**
  * Genera números aleatorios entre 0 y el número máximo de verbos de la lista.
  * Genera tantos números como cantidad de verbos que quiera completar el usuario.
  * Devuelve esos números en un array.
  */
-function createRandomNumbersArray($verbsAmountInput){
+function createRandomNumbersArray($a_selectedVerbsAmount){
    $a_randomNumbers = array();
    $cont = 0;
    do {
@@ -94,19 +32,18 @@ function createRandomNumbersArray($verbsAmountInput){
          $cont++;
       } 
 
-   } while ($cont < $verbsAmountInput );
+   } while ($cont < $a_selectedVerbsAmount );
    return $a_randomNumbers;
 };
-
 
 /**
  * Genera números aleatorios entre 0 y 3 sin repetir y los introduce en un arrays.
  * Genera la cantidad indicada por el usuario (1 a 4) y devuelve esos arrays.
  * @param {num} Nivel de dificultad y por tanto huecos que van a aparecer en cada verbo. 
  */
-function createVerbsGasps($difficultyLevel){
+function createVerbsGasps($a_selectedGaspsAmount){
    
-   $cont = $difficultyLevel;
+   $cont = $a_selectedGaspsAmount;
    $a_gasps = array();
    
    do {
@@ -124,16 +61,103 @@ function createVerbsGasps($difficultyLevel){
 /**
  * Función que completa el array de índices.
  */
-function fillIndexArray($verbsAmountInput, $difficultyLevel){
+function fillIndexArray($a_selectedVerbsAmount, $a_selectedGaspsAmount){
    //Array que le asignamos la función que genera array con números aleatorios.
-   $a_randomNumbers = createRandomNumbersArray($verbsAmountInput);
+   $a_randomNumbers = createRandomNumbersArray($a_selectedVerbsAmount);
 
    for ($i=0; $i < count($a_randomNumbers); $i++) {
       $indexNumber = $a_randomNumbers[$i];
-   $a_indexes[$indexNumber] = createVerbsGasps($difficultyLevel);
+   $a_indexes[$indexNumber] = createVerbsGasps($a_selectedGaspsAmount);
    }
    return $a_indexes;
 };
 
-echo(var_dump(fillIndexArray($verbsAmountInput, $difficultyLevel)));
+//echo(var_dump(fillIndexArray($a_selectedVerbsAmount, $a_selectedGaspsAmount)));
+
+
+function(){
+
+};
+
+//VALIDACIÓN DE DATOS
+//Arrays vacíos donde luego se guardarán las opciones elegidas por el usuario
+$a_selectedVerbsAmount = [];
+$a_selectedGaspsAmount = [];
+$f_processForm = false;
+$f_error = false;
+
+
+
+//Se comprueban todos los datos introducidos
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+   $f_processForm = true;
+
+   if (isset($_POST['verbsAmount'])) {
+   $a_selectedVerbsAmount = $_POST['verbsAmount'];
+}
+
+if (isset($_POST['level'])) {
+   $a_selectedGaspsAmount = $_POST['level'];
+}
+};
+
+if ($f_error) {
+   $f_processForm = false;
+}
+
+// $a_selectedGaspsAmount = $a_selectedGaspsAmount[0];
+// $a_selectedVerbsAmount = $a_selectedVerbsAmount[0];
+
+//////////////////////HTML
 ?>
+<!DOCTYPE HTML>
+<html lang="es">
+
+<head>
+    <style>
+        .error {
+            color: "red";
+        }
+    </style>
+</head>
+
+<body>
+
+<?php
+if (!$f_processForm) { ?>  
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <h1>Test Verbos Irregulares en Inglés</h1> 
+<br>
+
+<label for='level'>Elige el nivel según dificultad </label>
+<select name='level' id='level'>
+<option value='1'>1. Completar un hueco</option>
+<option value='2'>2. Completar dos huecos</option>
+<option value='3'>3. Completar tres huecos</option>
+<option value='4'>4. Completar todos los huecos</option>
+</select><br><br>
+
+<label for='verbsAmount'>Elige la cantidad de verbos </label>
+<select name='verbsAmount' id='verbsAmount'>;
+<?php
+   for ($i = 1; $i <= 20; $i++) {
+      echo ("<option value='$i'" . $i . "'>" . $i . "</option>");
+   }
+   echo ("</select><br>");?>
+
+<br>
+<input type='submit' value='Enviar' id='submit'>
+
+   </form>
+<?php
+} // Muestra verbos
+else {
+   // echo("se procesa");
+   // var_dump($a_selectedVerbsAmount);
+   // var_dump("Dificultad: " .  $a_selectedGaspsAmount);
+
+   echo(var_dump(fillIndexArray($a_selectedVerbsAmount, $a_selectedGaspsAmount)));
+}
+?>
+</body>
+</html>
