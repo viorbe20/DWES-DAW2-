2fc63d6c467669b7d82888a9b04e2ec6c7855e3d
Virginia-Ordoño-Bernier <viorbe20@gmail.com>
2021-10-26 20:56:15 +0200

    ex. verbs: array and indexarray creation

--- ud3/ud3_act_form/ej5/index.php
+++ ud3/ud3_act_form/ej5/index.php
@@ -5,7 +5,7 @@
  * teniendo el cuenta la dificultad y el número de verbos que aparecerán.
  * @author: Virginia Ordoño Bernier
  * @date: October 2021st
- * http://192.168.10.10/ud3/ud3_act_clase/ej5/index.php
+ * http://192.168.10.10/ud3/ud3_act_form/ej5/
  */
 
  //Estilo
@@ -19,97 +19,14 @@
  <h1>Test Verbos Irregulares en Inglés</h1>
  EOD;
 
+// //Archivo que contiene array con los verbos irregulares
+require __DIR__ . "/php/getIrregularVerbs.php";
 
- //Array que contiene los verbos
- $irregularVerbs = array (
-   array(
-      "infinitive" => "Arise",
-      "simplePast" => "Arose",
-      "pastParticiple" => "Arisen",
-      "spanish" => "Surgir",
-      "audio"=> "arise.mp3"
-   ),
-     array(
-        "infinitive" => "Awake",
-        "simplePast" => "Awoke",
-        "pastParticiple" => "Awoken",
-        "spanish" => "Despertarse",
-        "audio"=> "awake.mp3"
-     ),
-     array(
-        "infinitive" => "Be",
-        "simplePast" => "Were",
-        "pastParticiple" => "Been",
-        "spanish" => "Ser",
-        "audio"=> "be.mp3",
-     ),
-     array(
-      "infinitive" => "Bear",
-      "simplePast" => "Bore",
-      "pastParticiple" => "Born",
-      "spanish" => "Soportar",
-      "audio"=> "bear.mp3",
-   ),
-   array(
-      "infinitive" => "Beat",
-      "simplePast" => "Beat",
-      "pastParticiple" => "Beaten",
-      "spanish" => "Golpear",
-      "audio"=> "Beat.mp3",
-   ),
-   array(
-      "infinitive" => "Become",
-      "simplePast" => "Became",
-      "pastParticiple" => "Become",
-      "spanish" => "Llegar a ser",
-      "audio"=> "become.mp3",
-   ),
-   array(
-      "infinitive" => "Begin",
-      "simplePast" => "Began",
-      "pastParticiple" => "Begun",
-      "spanish" => "Empezar",
-      "audio"=> "begin.mp3",
-   ),
-   array(
-      "infinitive" => "Bend",
-      "simplePast" => "Bent",
-      "pastParticiple" => "Bent",
-      "spanish" => "Doblar",
-      "audio"=> "bend.mp3",
-   ),
-   array(
-      "infinitive" => "Bet",
-      "simplePast" => "Bet",
-      "pastParticiple" => "Bet",
-      "spanish" => "Apostar",
-      "audio"=> "bet.mp3",
-   ),
-   array(
-      "infinitive" => "Bind",
-      "simplePast" => "Bound",
-      "pastParticiple" => "Bound",
-      "spanish" => "Atajar",
-      "audio"=> "bind.mp3",
-   ),
-   array(
-      "infinitive" => "Bid",
-      "simplePast" => "Bid",
-      "pastParticiple" => "Bid",
-      "spanish" => "Pujar",
-      "audio"=> "bid.mp3",
-   ),
-   array(
-      "infinitive" => "Bite",
-      "simplePast" => "Bit",
-      "pastParticiple" => "Bitten",
-      "spanish" => "Morder",
-      "audio"=> "bite.mp3",
-   ),
-    
- );
+$difficultyLevel = 2;
+$verbsAmountInput = 6;
+$a_indexes = array();
 
- 
+
  /**
   * Función que genera y muestra 20 números para que el usuario elija cantidad de verbos
   */
@@ -140,25 +57,83 @@ function randomNumbers() {
       echo ("</select><br>");
 };
 
+// /**
+//  * Función que envía el formulario inicial
+//  */
+// function submitSelection(){
+//    echo("<br><input type='submit' value='Enviar' id='submit'>");
+// };
+
+// /**
+//  * Función que invoca las funciones necesarias para mostrar el primer formulario por pantalla
+//  */
+// function showFirstForm(){
+//    randomNumbers();
+//    difficultyLevel();
+//    submitSelection();
+// };
+
+// //Salida por pantalla
+// showFirstForm();
+
+
+
 /**
- * Función que envía el formulario inicial
+ * Genera números aleatorios entre 0 y el número máximo de verbos de la lista.
+ * Genera tantos números como cantidad de verbos que quiera completar el usuario.
+ * Devuelve esos números en un array.
  */
-function submitSelection(){
-   echo("<br><input type='submit' value='Enviar' id='submitButton'>");
+function createRandomNumbersArray($verbsAmountInput){
+   $a_randomNumbers = array();
+   $cont = 0;
+   do {
+      $randomNumber = rand(0, count(getIrregularVerbs())-1);
+
+      if (in_array($randomNumber, $a_randomNumbers)==false) {
+         array_push($a_randomNumbers, $randomNumber);
+         $cont++;
+      } 
+
+   } while ($cont < $verbsAmountInput );
+   return $a_randomNumbers;
+};
+
+
+/**
+ * Genera números aleatorios entre 0 y 3 sin repetir y los introduce en un arrays.
+ * Genera la cantidad indicada por el usuario (1 a 4) y devuelve esos arrays.
+ * @param {num} Nivel de dificultad y por tanto huecos que van a aparecer en cada verbo. 
+ */
+function createVerbsGasps($difficultyLevel){
+   
+   $cont = $difficultyLevel;
+   $a_gasps = array();
+   
+   do {
+      $randomNumber = rand(0, 3);
+      if (in_array($randomNumber, $a_gasps)==false) {
+         array_push($a_gasps, $randomNumber);
+         $cont--;
+      } 
+      
+   } while ($cont > 0);
+   return $a_gasps;
+
 };
 
 /**
- * Función que invoca las funciones necesarias para mostrar el primer formulario por pantalla
+ * Función que completa el array de índices.
  */
-function showFirstForm(){
-   randomNumbers();
-   difficultyLevel();
-   submitSelection();
+function fillIndexArray($verbsAmountInput, $difficultyLevel){
+   //Array que le asignamos la función que genera array con números aleatorios.
+   $a_randomNumbers = createRandomNumbersArray($verbsAmountInput);
+
+   for ($i=0; $i < count($a_randomNumbers); $i++) {
+      $indexNumber = $a_randomNumbers[$i];
+   $a_indexes[$indexNumber] = createVerbsGasps($difficultyLevel);
+   }
+   return $a_indexes;
 };
 
-//Salida por pantalla
-showFirstForm();
-
-
-
+echo(var_dump(fillIndexArray($verbsAmountInput, $difficultyLevel)));
 ?>
